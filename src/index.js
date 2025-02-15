a62a0fa96828e5f551d7b65a088bd72aae52e18f
anliben <anliben.yayoi@gmail.com>
2025-02-15 12:51:48 -0300

    deploy new config

--- src/index.js
+++ src/index.js
@@ -1,54 +1,60 @@
-addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request))
-})
+// addEventListener('fetch', event => {
+//     event.respondWith(handleRequest(event.request))
+// })
 
-async function handleRequest(request) {
-    try {
-		const headers = Object.fromEntries(request.headers.entries());
-		const fingerprint = await getFingerprint(headers)
+export default {
+	async fetch(request, env) {
+		const { pathname } = new URL(request.url);
+		console.log(pathname);
 
-		await saveToFingerprintDb(fingerprint)
 
-        return new Response(JSON.stringify({
-            success: true,
-            message: 'Fingerprint registrado com sucesso',
-            fingerprintId: fingerprint.fingerprint_id
-        }), {
-            headers: {
-                'content-type': 'application/json;charset=UTF-8'
-            }
-        })
-    } catch (error) {
-        console.error('Erro:', error)
-        return new Response(JSON.stringify({
-            success: false,
-            message: 'Erro ao registrar fingerprint',
-            error: error.message
-        }), {
-            status: 500,
-            headers: {
-                'content-type': 'application/json;charset=UTF-8'
-            }
-        })
-    }
+		try {
+			const headers = Object.fromEntries(request.headers.entries());
+			const fingerprint = await getFingerprint(headers)
+	
+			await saveToFingerprintDb(fingerprint, env)
+	
+			return new Response(JSON.stringify({
+				success: true,
+				message: 'Fingerprint registrado com sucesso',
+				fingerprintId: fingerprint.fingerprint_id
+			}), {
+				headers: {
+					'content-type': 'application/json;charset=UTF-8'
+				}
+			})
+		} catch (error) {
+			console.error('Erro:', error)
+			return new Response(JSON.stringify({
+				success: false,
+				message: 'Erro ao registrar fingerprint',
+				error: error.message
+			}), {
+				status: 500,
+				headers: {
+					'content-type': 'application/json;charset=UTF-8'
+				}
+			})
+		}
+	}
 }
 
 async function getFingerprint(request) {
-    const fingerprint = {
-        fingerprint_id: crypto.randomUUID(),
-        browser_type: request['user-agent'],
-        os: request['sec-ch-ua-platform'],
-        host: request['host'],
-        language: request['accept-language'],
-        timezone_offset: new Date().getTimezoneOffset()
-    }
-    
-    return fingerprint
+	const fingerprint = {
+		fingerprint_id: crypto.randomUUID(),
+		browser_type: request['user-agent'],
+		os: request['sec-ch-ua-platform'],
+		host: request['host'],
+		language: request['accept-language'],
+		timezone_offset: new Date().getTimezoneOffset()
+	}
+
+	return fingerprint
 }
 
-async function saveToFingerprintDb(fingerprint) {
-    const db = await D1DATABASE.prepare(`
-        INSERT INTO fingerprints (
+async function saveToFingerprintDb(fingerprint, env) {
+	const db = await env.DB.prepare(`
+        INSERT INTO example (
             fingerprint_id,
             browser_type,
             os,
@@ -57,15 +63,15 @@ async function saveToFingerprintDb(fingerprint) {
             timezone_offset
         ) VALUES (?, ?, ?, ?, ?, ?)
     `)
-    
-    await db.bind(
-        fingerprint.fingerprint_id,
-        fingerprint.browser_type,
-        fingerprint.os,
-        fingerprint.host,
-        fingerprint.language,
-        fingerprint.timezone_offset
-    ).run()
-    
-    return true
+
+	await db.bind(
+		fingerprint.fingerprint_id,
+		fingerprint.browser_type,
+		fingerprint.os,
+		fingerprint.host,
+		fingerprint.language,
+		fingerprint.timezone_offset
+	).run()
+
+	return true
 }
---
+++ src/schemas/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS example (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    fingerprint_id TEXT UNIQUE NOT NULL,
+    browser_type TEXT,
+    os TEXT,
+    host TEXT,
+    language TEXT,
+    timezone_offset INTEGER,
+);
